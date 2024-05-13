171c3cbbf2081014729bb738e59bf441fc434b88
Kiminori Mizushino <k_mizushino@ewings.biz>
2024-05-13 20:04:13 +0900

    AnimatorBinder追加

---
+++ Assets/uDataBinder/Runtime/Binder/AnimatorBinder.cs
@@ -0,0 +1,57 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace uDataBinder.Binder
+{
+    public enum AnimatorBinderType
+    {
+        Bool,
+        Float,
+        Int,
+    }
+
+    [DefaultExecutionOrder(500)]
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorBinder : DataBinder
+    {
+        protected Animator _animator;
+        protected Animator Animator => this == null || _animator != null ? _animator : _animator = GetComponent<Animator>();
+
+        [SerializeField] protected AnimatorBinderType _type = AnimatorBinderType.Int;
+        [SerializeField] protected string _name;
+        [SerializeField] protected string _value;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            DataBinding.Register(_value, this);
+        }
+
+        public override void Release()
+        {
+            DataBinding.Unregister(this);
+            _animator = null;
+
+            base.Release();
+        }
+
+        protected override Task RebuildAsync()
+        {
+            Debug.Log("RebuildAsync");
+            switch (_type)
+            {
+                case AnimatorBinderType.Bool:
+                    Animator.SetBool(_name, DataBinding.GetValue<bool>(_value));
+                    break;
+                case AnimatorBinderType.Float:
+                    Animator.SetFloat(_name, DataBinding.GetValue<float>(_value));
+                    break;
+                case AnimatorBinderType.Int:
+                    Animator.SetInteger(_name, DataBinding.GetValue<int>(_value));
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
