a1765e03dfb6e3a8c6c7c7c887cc8bd15312ec85
Kiminori Mizushino <k_mizushino@ewings.biz>
2024-05-21 12:18:29 +0900

    小数部分を取り出した際、文字列にするように変更

--- Assets/uDataBinder/Runtime/Converter/ToDecimalPart.cs
+++ Assets/uDataBinder/Runtime/Converter/ToDecimalPart.cs
@@ -11,19 +11,20 @@ namespace uDataBinder.Converter
 
         protected override Task<object> ConvertAsync(object v)
         {
+            var zeros = new string('0', _digits);
             switch (v)
             {
                 case int:
                 case long:
-                    return Task.FromResult(0 as object);
+                    return Task.FromResult(zeros as object);
                 case float f:
-                    return Task.FromResult((int)Math.Floor((f - (int)f) * Mathf.Pow(10, _digits)) as object);
+                    return Task.FromResult(Math.Floor((f - (int)f) * Mathf.Pow(10, _digits)).ToString(zeros) as object);
                 case double d:
-                    return Task.FromResult((int)Math.Floor((d - (int)d) * Mathf.Pow(10, _digits)) as object);
+                    return Task.FromResult(Math.Floor((d - (int)d) * Mathf.Pow(10, _digits)).ToString(zeros) as object);
             }
 
             var value = DataBinding.ConvertValue<float>(v);
-            return Task.FromResult((int)Math.Floor((value - (int)value) * Mathf.Pow(10, _digits)) as object);
+            return Task.FromResult(Math.Floor((value - (int)value) * Mathf.Pow(10, _digits)).ToString(zeros) as object);
         }
     }
 }
